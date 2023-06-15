6c464ac85ef39e9576b0f17748f1f4f3201799e3
Abdelhamid Soliman <abdelhamidfg@gmail.com>
2023-06-15 13:45:12 +0400

    Update dispatcher.java

--- Integrations/dispatcher/dispatcher.java
+++ Integrations/dispatcher/dispatcher.java
@@ -1,12 +1,13 @@
 // camel-k: language=java property-file=kafka.properties
 // camel-k: dependency=camel:gson
-
+// camel-k: dependency=camel:jdbc
+// camel-k: dependency=mvn:io.quarkus:quarkus-jdbc-postgresql
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.processor.idempotent.kafka.KafkaIdempotentRepository;
 import org.apache.camel.support.processor.idempotent.MemoryIdempotentRepository;
 
-public class IdempotentOrderConsumer extends RouteBuilder {
+public class dispatcher extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
@@ -15,12 +16,20 @@ public class IdempotentOrderConsumer extends RouteBuilder {
 
     MemoryIdempotentRepository memoryIdempotentRepository =  new MemoryIdempotentRepository(); 
          
-        from("kafka:orders-duplicate-topic")
+      from("kafka:order-created-event")
         .unmarshal().json(JsonLibrary.Gson)
-        .idempotentConsumer(simple("${body[orderId]}"), memoryIdempotentRepository)    
-        .log("Order Sent: ${body[orderId]}")
-        .marshal().json(JsonLibrary.Gson)
-        .to("kafka:orders-filtered-topic");
+        .idempotentConsumer(simple("${body[eventId]}"), memoryIdempotentRepository)    
+        .log("Order created with eventID: ${body[eventId]}")
+        .setBody(constant("SELECT app_id FROM  where  product_id=12"))
+        .to("jdbc:default")   
+        .split(body()).streaming().parallelProcessing()
+        .log("${body}")
+          //call 3scale api 
+        //.to("http://sampleurl.com?bridgeEndpoint=true&throwExceptionOnFailure=false")
+          
+                 
+        --.marshal().json(JsonLibrary.Gson)
+        --.to("kafka:orders-filtered-topic");
     }
 
 }
